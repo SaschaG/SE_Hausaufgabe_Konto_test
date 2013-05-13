035e976e99de934c3e38190b062baa3f1c99bd03
Sascha G <sushi.genji@hotmail.de>
2013-05-13 14:20:38 +0200

    made the structure of the test - method names and so on

--- Test/Konto_Test.java
+++ Test/Konto_Test.java
@@ -5,5 +5,36 @@ import org.junit.Test;
 
 public class Konto_Test {
 
+	Konto konto;
+
+	@Before
+	public void initialize() {
+		// Als erstes ein Konto erstellen
+		konto = new Konto(2000);
+	}
+
+	@Test
+	public void Startguthaben() {
+	}
+
+	@Test
+	public void einzahlen() {
+
+	}
+
+	@Test
+	public void abheben_ausreichend() {
+
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void abheben_ungenügent() {
+
+	}
+
+	@Test
+	public void mehrfach_einzahlen_komplett_abheben() {
+
+	}
 
 }
